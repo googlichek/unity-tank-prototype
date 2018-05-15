39d68ecc6c03b18a9f954e5eeed0bcf7b94589d2
Goga <googlichek.one@gmail.com>
2018-05-15 19:26:44 +0300

    Cleaned up scene loader a little.

---
+++ Assets/Scripts/SceneLoadingHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadingHandler : MonoBehaviour
+{
+	public void LoadScene(int sceneIndex)
+	{
+		if (sceneIndex < 0 ||
+			sceneIndex > SceneManager.sceneCountInBuildSettings - 1) return;
+
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void LoadNextScene()
+	{
+		var sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		if (sceneIndex >= SceneManager.sceneCountInBuildSettings - 1) return;
+
+		sceneIndex++;
+		SceneManager.LoadScene(sceneIndex);
+	}
+}
