c6523c34696fbfc05ed7b252874df7345828aa46
Goga <googlichek.one@gmail.com>
2018-05-15 23:02:26 +0300

    Saving current progress.

---
+++ Assets/_Project/Scripts/Handlers/InputHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TankProto
+{
+	public class InputHandler : MonoBehaviour
+	{
+		void FixedUpdate()
+		{
+			MoveTank();
+			ChangeWeapon();
+		}
+
+		private void MoveTank()
+		{
+		}
+
+		private void ChangeWeapon()
+		{
+		}
+	}
+}
