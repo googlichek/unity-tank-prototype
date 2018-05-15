a79cc6934fa2342c12b358b4c9d4cc5d4c77b581
Goga <googlichek.one@gmail.com>
2018-05-15 19:20:19 +0300

    Implemented basic scene loading.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	private GameManager _instance;
+
+	void Awake()
+	{
+		HandleGameManagerInitialization();
+	}
+
+	public void LoadScene(int sceneIndex)
+	{
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	public void LoadNextScene()
+	{
+		int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		if (sceneIndex >= SceneManager.sceneCountInBuildSettings - 1) return;
+
+		sceneIndex++;
+		SceneManager.LoadScene(sceneIndex);
+	}
+
+	private void HandleGameManagerInitialization()
+	{
+		if (_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			_instance = this;
+		}
+	}
+}
