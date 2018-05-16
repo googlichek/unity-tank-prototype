361980de89dfb871589d1b72f1c6d0a024567b2f
Goga <googlichek.one@gmail.com>
2018-05-16 14:23:56 +0300

    Restricted player movement to insides of arena.

--- Assets/_Project/Scripts/Data/GlobalVariables.cs
+++ Assets/_Project/Scripts/Data/GlobalVariables.cs
@@ -5,5 +5,9 @@
 		public const string HorizontalAxis = "Horizontal";
 
 		public const string VerticalAxis = "Vertical";
+
+		public const int EnvironmentLayer = 8;
+
+		public const int CharacterLayer = 9;
 	}
 }
--- Assets/_Project/Scripts/Handlers/InputHandler.cs
+++ Assets/_Project/Scripts/Handlers/InputHandler.cs
@@ -8,6 +8,14 @@ namespace TankProto
 		[SerializeField] [Range(0, 100)] private float _movementSpeed = 0f;
 		[SerializeField] [Range(0, 100)] private float _rotationSpeed = 0f;
 
+		[Header("Obstacle Detection Variables")]
+		[SerializeField] [Range(0, 3)] private float _castHeight = 1;
+		[SerializeField] [Range(0, 10)] private float _rayDistance = 4;
+		[SerializeField] [Range(0, 1)] private float _sphereRadius = 0.5f;
+
+		private const int LayerMask =
+			1 << GlobalVariables.EnvironmentLayer | 1 << GlobalVariables.CharacterLayer;
+
 		void FixedUpdate()
 		{
 			HandleMovement();
@@ -19,6 +27,22 @@ namespace TankProto
 		{
 			float deltaZ = Input.GetAxis(GlobalVariables.VerticalAxis);
 			Vector3 offset = new Vector3(0, 0, deltaZ * _movementSpeed);
+
+			if (offset == Vector3.zero) return;
+
+			//Vector3 rayOrigin =
+			//	new Vector3(transform.position.x, _castHeight, transform.position.z);
+
+			//RaycastHit hitTarget;
+
+			//Ray ray =
+			//	deltaZ < 0 ?
+			//		new Ray(rayOrigin, -transform.forward) :
+			//		new Ray(rayOrigin, transform.forward);
+
+			//bool isHit =
+			//	Physics.SphereCast(ray, _sphereRadius, out hitTarget, _rayDistance, LayerMask);
+			//if (isHit && hitTarget.distance <= _rayDistance) return;
 
 			transform.Translate(offset * Time.deltaTime);
 		}
